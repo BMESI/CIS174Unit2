ff61c66691e80c738e2f514a443f6da5c9ecd836
bmesi <bmesinovic@dmacc.edu>
2019-02-06 18:15:59 -0600

    Week 4 - Due 2/6 by 11:59 pm --------------------------------- Assignment 4.1 | Add a menu Assignment 4.2 | Add Layout Page

---
+++ Unit2A3Mesinovic/Controllers/PuzzlersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Unit2A3Mesinovic.Controllers
+{
+    public class PuzzlersController : Controller
+    {
+        // GET: Puzzlers
+        public ActionResult PuzzlersPage()
+        {
+            return View();
+        }
+    }
+}
